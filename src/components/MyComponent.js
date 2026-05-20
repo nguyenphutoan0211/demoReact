d8380bc734ab58d0e835c0433be024d90dafda88
toannp <ngtoan554@gmail.com>
2026-05-20 23:46:38 +0700

    component1

---
+++ src/components/MyComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+  render() {
+    return (
+        <div>My Component 1</div>
+        );
+    }
+}
+
+export default MyComponent;
